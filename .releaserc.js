fb18068d5acb1fd3decd292dd750be85df019d63
Simon Golms <development@gol.ms>
2026-01-09 15:50:24 +0100

    chore(release): stop making commit during release process
    - follow semantic-release recommendation and reduce significant complexity, see: https://semantic-release.gitbook.io/semantic-release/support/faq#making-commits-during-the-release-process-adds-significant-complexity
    - update CHANGELOG to find list of changes in the GitHub Releases and remove redundancy.
    - uninstall package @semantic-release/changelog@6.0.3
    - uninstall package @semantic-release/git@10.0.1

--- .releaserc.js
+++ .releaserc.js
@@ -1,24 +1,16 @@
 /** @type {import('semantic-release').Options}  */
 module.exports = {
   branches: [
-    "master",
+    'master',
     {
-      name: "next",
+      name: 'next',
       prerelease: true,
     },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "README.md", "package.json", "package-lock.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-    "@semantic-release/github",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
   ],
 };
