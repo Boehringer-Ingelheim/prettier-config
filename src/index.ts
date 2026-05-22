c539ecb3ee7baaf2aedecae012ec5d98b52fffbd
Simon Golms <development@gol.ms>
2026-05-22 14:58:25 +0200

    fix(types): correct type imports and ensure plugin compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Config, Plugin } from 'prettier';
+import type { Plugin, Config as PrettierConfig } from 'prettier';
 import prettierPluginGroovy from 'prettier-plugin-groovy';
 import prettierPluginNginx from 'prettier-plugin-nginx';
 import prettierPluginPkg from 'prettier-plugin-pkg';
@@ -7,19 +7,21 @@ import { PRETTIER_PLUGIN_JSDOC_CONFIG, prettierPluginJsdoc } from './prettier-pl
 import { PRETTIER_PLUGIN_SQL_CONFIG, prettierPluginSql } from './prettier-plugin-sql.ts';
 
 const plugins = [
-  prettierPluginGroovy,
-  prettierPluginNginx as unknown as Plugin,
-  prettierPluginPkg,
+  prettierPluginGroovy as unknown as Plugin<unknown>,
+  prettierPluginNginx as unknown as Plugin<unknown>,
+  prettierPluginPkg as unknown as Plugin<unknown>,
   prettierPluginSh,
   prettierPluginSql,
   // Should be the last one to avoid conflicts
   prettierPluginJsdoc,
-] as const satisfies Config['plugins'];
+] as const satisfies Plugin[];
 
 const pluginConfigs = {
   ...PRETTIER_PLUGIN_JSDOC_CONFIG,
   ...PRETTIER_PLUGIN_SQL_CONFIG,
-} as const satisfies Omit<Config, 'plugins'>;
+} as const satisfies Omit<PrettierConfig, 'plugins'>;
+
+export type Config = PrettierConfig & typeof pluginConfigs & { plugins: typeof plugins };
 
 export default {
   plugins,
@@ -27,4 +29,4 @@ export default {
   printWidth: 120,
   singleQuote: true,
   checkIgnorePragma: true,
-} as const satisfies Config;
+} as const satisfies PrettierConfig;
